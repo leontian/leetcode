eafe069cccfc9ef491cbbfe6d9ff6cf9fb8df2f5
leontian <l.tian.email@gmail.com>
2015-08-11 01:22:31 -0400

    206, better solution

--- p_206.py
+++ p_206.py
@@ -7,15 +7,34 @@
 class Solution:
     # @param {ListNode} head
     # @return {ListNode}
+    # def reverseList(self, head):
+    #     cur = head
+    #     l = list()
+    #     while cur:
+    #         l.append(cur)
+    #         cur = cur.next
+    #     for i in xrange(len(l)-1, 0, -1):
+    #         l[i].next = l[i-1]
+    #     if len(l) > 0:
+    #         l[0].next = None
+    #         return l[len(l)-1]
+    #     return None
     def reverseList(self, head):
-        cur = head
-        l = list()
+        if not head:
+            return None
+        prev = head
+        cur = head.next
+        if not cur:
+            return head
+        nxt = None
         while cur:
-            l.append(cur)
-            cur = cur.next
-        for i in xrange(len(l)-1, 0, -1):
-            l[i].next = l[i-1]
-        if len(l) > 0:
-            l[0].next = None
-            return l[len(l)-1]
-        return None
+            nxt = cur.next
+            cur.next = prev
+            if not nxt:
+                nxt = cur
+                break
+            prev = cur
+            cur = nxt
+            nxt = nxt.next
+        head.next = None
+        return nxt
