90b6d2185c8b4ed9f07184d9ac5760050b94cabe
leontian <l.tian.email@gmail.com>
2016-01-14 16:05:55 -0500

    58

---
+++ p_058.py
@@ -0,0 +1,11 @@
+# Length of Last Word
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = s.split()
+        if len(s) == 0:
+            return 0
+        return len(s[-1])
